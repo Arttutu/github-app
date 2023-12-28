e92b80f3d6ed00e8e0c5b45d04a8296deaedc00a
Arttutu <arthurtt08@gmail.com>
2023-12-28 10:01:38 -0300

    adicionando cores do projeto e tipografia

---
+++ src/Components/Variaveis/index.jsx
@@ -0,0 +1,17 @@
+// Modo Claro Cores
+
+export const cor1 = "#0079FF";
+export const cor2 = "#697C9A";
+export const cor3 = "#4B6A9B";
+export const cor4 = "#2B3442";
+export const cor5 = "#F6F8FF";
+export const cor6 = "#FEFEFE";
+
+// Modo Escuro cores
+
+export const cor7 = "#FFFFFF";
+export const cor8 = "#141D2F";
+export const cor9 = "#1E2A47";
+
+// Tipografia
+export const font = "'Space Mono', monospace;";
