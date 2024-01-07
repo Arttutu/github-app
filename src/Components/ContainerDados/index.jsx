5998f43ab67658506283cff013b99e754984b17a
Arttutu <arthurtt08@gmail.com>
2024-01-07 15:33:03 -0300

    criando container info ,arrumando arquivos de estilos, falta fazer responsividade e arrumar alguns detalhes

--- src/Components/ContainerDados/index.jsx
+++ src/Components/ContainerDados/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { Container } from "./style";
+import { Container, ContainerInfo, ContainerUser } from "./style";
+import locationIcon from "./icon-location.svg";
+import webIcon from "./icon-website.svg";
+import twitterIcon from "./icon-twitter.svg";
+import companyIcon from "./icon-company.svg";
 
 export default function ContainerDados({ usuario }) {
   return (
@@ -7,15 +11,17 @@ export default function ContainerDados({ usuario }) {
       {usuario ? (
         <>
           <div>
-            <img
-              src={usuario.avatar_url}
-              alt={`Avatar de ${usuario.login}`}
-            ></img>
+            <div>
+              <img
+                src={usuario.avatar_url}
+                alt={`Avatar de ${usuario.login}`}
+              />
+            </div>
             <div>
               <h2>{usuario.name}</h2>
               <span>@{usuario.login}</span>
               <p>{usuario.bio}</p>
-              <div>
+              <ContainerUser>
                 <div>
                   <h3>Repositorio</h3>
                   <p>{usuario.public_repos}</p>
@@ -28,7 +34,45 @@ export default function ContainerDados({ usuario }) {
                   <h3>Seguindo</h3>
                   <p>{usuario.following}</p>
                 </div>
-              </div>
+              </ContainerUser>
+              <ContainerInfo>
+                <div>
+                  <div>
+                    <img src={locationIcon} />
+                    <h4>
+                      {usuario.location != null
+                        ? usuario.location
+                        : " não disponível"}
+                    </h4>
+                  </div>
+                  <div>
+                    <img src={twitterIcon} />
+                    <h4>
+                      {usuario.twitter_username != null
+                        ? usuario.twitter_username
+                        : " não disponível"}
+                    </h4>
+                  </div>
+                </div>
+              </ContainerInfo>
+              <ContainerInfo>
+                <div>
+                  <div>
+                    <img src={webIcon} />
+                    <h4>
+                      {usuario.blog != null ? usuario.blog : " não disponível"}
+                    </h4>
+                  </div>
+                  <div>
+                    <img src={companyIcon} />
+                    <h4>
+                      {usuario.company != null
+                        ? usuario.company
+                        : " não disponível"}
+                    </h4>
+                  </div>
+                </div>
+              </ContainerInfo>
             </div>
           </div>
         </>
--- src/Components/ContainerDados/style.jsx
+++ src/Components/ContainerDados/style.jsx
@@ -7,15 +7,18 @@ export const Container = styled.section`
   margin-top: 24px;
   height: 444px;
   width: 730px;
+  padding-top: 48px;
   > div {
-    display: flex;
-    align-items: top;
-
     img {
       width: 117px;
       height: 117px;
       border-radius: 50%;
+      margin: 0px 32px;
     }
+  }
+  > div {
+    display: flex;
+    align-items: top;
     > div {
       display: flex;
       flex-direction: column;
@@ -37,30 +40,56 @@ export const Container = styled.section`
         font-size: 15px;
         color: ${(props) => props.theme.colors.corTextoDois};
       }
-      > div {
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        padding-left: 32px;
-        gap: 100px;
-        border-radius: 10px;
-        background-color: ${(props) => props.theme.colors.corPrimeira};
-        width: 480px;
-        height: 85px;
-        > div {
-          display: flex;
-          flex-direction: column;
-          gap: 0.5em;
-          h3 {
-            font-family: ${Font};
-            font-size: 13px;
-            color: ${(props) => props.theme.colors.corTextoDois};
-          }
-          p {
-            font-weight: bold;
-            font-size: 22px;
-          }
-        }
+    }
+  }
+`;
+export const ContainerUser = styled.div`
+  width: 480px;
+  height: 85px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding-left: 32px;
+  gap: 100px;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.colors.corPrimeira};
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    h3 {
+      font-family: ${Font};
+      font-size: 13px;
+      color: ${(props) => props.theme.colors.corTextoDois};
+    }
+    p {
+      font-weight: bold;
+      font-size: 22px;
+    }
+  }
+`;
+export const ContainerInfo = styled.div`
+  margin-top: 10px;
+  > div {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: space-between;
+    > div {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      h4 {
+        font-family: ${Font};
+        font-size: 15px;
+        color: ${(props) => props.theme.colors.corTexto};
+      }
+      img {
+        margin: 0px;
+        width: 30px;
+        height: 30px;
+        fill: ${(props) => props.theme.colors.corTexto};
       }
     }
   }
